2d31e9af2d89cc4189682dd50acc08b09e2e1ea8
Shahar Dahan <dahshahar@gmail.com>
2020-12-24 11:27:14 -0500

    Have homepage slideshow be controlled manually

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -1,14 +1,41 @@
-var slideIndex = 0;
-showSlides();
+var timeout;
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("banner_slides");
+var slideIndex = 0;
+plusSlides(1);
+
+function plusSlides(value) {
+  var slides = getSlides();
+  var dots = document.getElementsByClassName("dot");
+  hideAllSlides(slides);
+  slideIndex += value;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  } else if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  resetTimeout();
+}
+
+function getSlides() {
+  return document.getElementsByClassName("banner_slides");
+}
+
+function hideAllSlides(slides) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 8000);
+}
+
+function currentSlide(value) {
+  plusSlides(value - slideIndex);
+}
+
+function resetTimeout() {
+  clearTimeout(timeout);
+  timeout = setTimeout(plusSlides.bind(null, 1), 8000);
 }
