2887caa2da8e87590b15d30144b6ba7a22210695
Shahar Dahan <dahshahar@gmail.com>
2020-12-24 11:48:00 -0500

    Always have home page be expanded

--- src/scripts/partners.js
+++
@@ -1,9 +0,0 @@
-function toggleVisibility(subject) {
-  var elementClasses = document.getElementById(subject).classList;
-
-  if (elementClasses.contains('unexpanded')) {
-    elementClasses.replace('unexpanded', 'expanded');
-  } else {
-    elementClasses.replace('expanded', 'unexpanded');
-  }
-}
