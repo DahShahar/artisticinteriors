11d1de4326749608be198f602a6e00f1d9936668
Shahar Dahan <dahshahar@gmail.com>
2020-12-29 14:51:24 -0500

    Gallery: on image click, open up a modal with larger image

--- src/gallery/scripts/lazy_load_images.js
+++ src/gallery/scripts/lazy_load_images.js
@@ -1,8 +1,10 @@
 var hasMoreImages = true;
 var currentImage = 0;
-var loadedImage = 0;
+var displayImage = 0;
 
 var timer;
+var modal = document.getElementById("modal");
+var modalImage = document.getElementById("modal-image");
 
 function getImages() {
   if (!hasMoreImages) {
@@ -13,7 +15,6 @@ function getImages() {
   var container = document.getElementById("image_container");
   var img = new Image();
   img.className = "gallery_photo_thumbnail";
-  img.onload = imageLoaded;
   img.onerror = outOfImages;
   img.onclick = clickedImage;
   img.src = `images/${IMAGE_BASE}_${currentImage}.jpg`;
@@ -26,20 +27,87 @@ function getImages() {
 function outOfImages() {
   this.style.display = "none";
   hasMoreImages = false;
+  currentImage--;
 }
 
-function imageLoaded() {
-  loadedImage++;
+function clickedImage(element) {
+  modal.style.display = "block";
+  modalImage.src = this.src;
+
+  // get the number of the currently displayed image based off the src
+  var imageNumber = this.src.split('\\').pop()
+    .split('/').pop()
+    .split('_').pop()
+    .split('.').shift();
+  displayImage = parseInt(imageNumber);
 }
 
-function clickedImage() {
-  if (this.clicked == null || this.clicked == false) {
-    this.clicked = true;
-    this.className = "gallery_photo";
+function changeImage(value) {
+  displayImage += value;
+
+  if (displayImage >= currentImage) {
+    displayImage = 0;
+  } else if (displayImage < 0) {
+    displayImage = currentImage - 1;
+  }
+
+  modalImage.src = `images/${IMAGE_BASE}_${displayImage}.jpg`;
+}
+
+function nextImage() {
+  changeImage(1);
+}
+
+function previousImage() {
+  changeImage(-1);
+}
+
+function hideModal() {
+  modal.style.display = "none";
+}
+
+function isEscapeKeyPress(event) {
+  if ("key" in event) {
+    return (event.key === "Escape" || event.key === "Esc");
   } else {
-    this.clicked = false;
-    this.className = "gallery_photo_thumbnail";
+    return event.keyCode === 27;
   }
 }
 
+function isLeftKeyPress(event) {
+  if ("key" in event) {
+    return event.key === "ArrowLeft"
+  } else {
+    return event.keyCode === 37;
+  }
+}
+
+function isRightKeyPress(event) {
+  if ("key" in event) {
+    return event.key === "ArrowRight"
+  } else {
+    return event.keyCode === 39;
+  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    hideModal();
+  }
+}
+
+window.onkeydown = function(event) {
+  event = event || window.event;
+
+  if (isEscapeKeyPress(event)) {
+    hideModal();
+  } else if (isLeftKeyPress(event)) {
+    previousImage();
+  } else if (isRightKeyPress(event)) {
+    nextImage();
+  }
+}
+
+// lazy load the images
 getImages();
